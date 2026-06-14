d8313bf1951e07e341b145eeed74757dee017b73
nemoirisgurl <noppanart5777@gmail.com>
2026-06-14 09:47:57 -0400

    feat: add recursive input

--- number_guess.sh
+++ number_guess.sh
@@ -6,8 +6,15 @@ PSQL="psql -U $DB_USER --d $DB_NAME -t -A -c"
 RN=$(( 1 + $RANDOM % 1000 ))
 
 GET_USERNAME() {
+  # get username
   echo -e "Enter your username:\n"
   read USERNAME
+  # if not found
+  if [[ -z "$USERNAME" ]]
+  then
+    # prompt again
+    GET_USERNAME 
+  fi 
 }
 
 GET_USERNAME
